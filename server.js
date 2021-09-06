f850d6cf4dbefb0ef191f6092ef7ac159bcb50bc
Leandro Braga <leantb00@gmail.com>
2021-09-06 18:46:48 -0300

    changed server file

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("batata");
+const express = require('express'); //requerer o módulo express, instalado no npm pelo através do nodejs.
+const {resolve} =require('path');
+const app = express();
+
+app.use('./', express.static(resolve(__dirname,'./build')))        //o express vai observar um diretório e vai servidr os arquivos que estão dentro do diretorio dentro dessa rota '/'.
+
+app.listen(process.env.PORT || 3000,(err) =>{
+   if  (err){
+       return console.log(err);
+   }
+   console.log("Tudo Está Funcionado Certinho")
+})
