e23b121e18b6dc103bacf59f12252b7ad3f6b71c
Julio Felix <julio.felix@deway.com.br>
2021-09-10 15:27:53 -0300

    feat: Prototype Facebook Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React, { useState } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import { Card, Image } from 'react-bootstrap';
 import logo from './logoTipoNightFun.png';
 import './App.css';
 import logoTipoFacebook from './logoTipoFacebook.png';
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState('');
+  
+  
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  } 
  return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +28,14 @@ function App() {
         <p>
           Seja Bem Vindo Ao NightFun.
         </p>
+        <FacebookLogin
+          appId="573188283694881"
+          autoLoad={true}
+          fields="name,email,picture"
+          scope="public_profile,email"
+          callback={responseFacebook}
+          icon="fa-facebook" 
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +45,17 @@ function App() {
            <img  src={logoTipoFacebook} alt="logoTipoFacebook.png" ></img> {/* logotipo Do facebook incluído como link */} 
         </a>
       </header>
+      {login &&
+            <Image src={picture} roundedCircle />
+          }
+      {login &&
+          <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>
+              {data.email}
+            </Card.Text>
+          </Card.Body>
+        }
     </div>
   );
 }
