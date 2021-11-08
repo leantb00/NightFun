6c779b327f352fee10b6d47332c3b0325f2a1080
Julio Felix <julio.felix@deway.com.br>
2021-11-08 19:39:37 -0300

    try fix Error Home

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express'); //requerer o módulo express, instalado no npm pelo através do nodejs.
-const {resolve} =require('path');
+const {resolve, join} =require('path');
 const app = express();
 
 app.use('/', express.static(resolve(__dirname,'./build')))        //o express vai observar um diretório e vai servidr os arquivos que estão dentro do diretorio dentro dessa rota '/'.
 
+if (process.env.NODE_ENV === 'production') {
+    const publicPath = join(__dirname, '../public');
+    app.use(express.static(publicPath));
+    app.use('*', express.static(publicPath));
+  }
 app.listen(process.env.PORT || 3000,(err) =>{
    if  (err){
        return console.log(err);
