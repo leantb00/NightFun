62c71cd567a68a8b0a9de0953352b164b3a647fb
Julio Felix <julio.felix@deway.com.br>
2021-11-08 19:45:20 -0300

    fix server js

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const app = express();
 
 app.use('/', express.static(resolve(__dirname,'./build')))        //o express vai observar um diretório e vai servidr os arquivos que estão dentro do diretorio dentro dessa rota '/'.
 
-if (process.env.NODE_ENV === 'production') {
-    const publicPath = join(__dirname, '../public');
-    app.use(express.static(publicPath));
-    app.use('*', express.static(publicPath));
-  }
+app.get('*', (req, res) => {                       
+    res.sendFile(resolve(__dirname, './public', 'index.html'));                               
+  });
 app.listen(process.env.PORT || 3000,(err) =>{
    if  (err){
        return console.log(err);
